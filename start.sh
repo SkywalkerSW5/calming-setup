9ac2cecfc95d323e1b07c01d109816bb65cbf1f4
SkywalkerSW5 <50605573+SkywalkerSW5@users.noreply.github.com>
2020-04-22 18:39:34 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -10,5 +10,14 @@ cd .compiled
 git clone https://github.com/SkywalkerSW5/calming-setup.git
 mv Arc-Clone /home/SkywalkerSW5/.themes
 cd
+cd .local/share/plank/themes/
+wget https://dllb2.pling.com/api/files/download/j/eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjE1NzM0OTAzOTciLCJ1IjpudWxsLCJsdCI6ImRvd25sb2FkIiwicyI6IjQ0NGRmMzc3ZjFjZTA3YmVkNGJkOGZkMTUzZmQyYWYyNWEzNmJiYTc0ZGZmMjRjNzMxODM3YjMxMWIyZTZhYTc2ZTA5MjA1NzAwYzE1N2JmNWU3MWYxZWNiNGQ4MDc3ZjAxMjYyNjU2MDQzNjZlODAzNGU4ZTJhNGRlMWVmMmFkIiwidCI6MTU4NzU4MDIzOCwic3RmcCI6ImUwNzEwNDVjN2Y5ZWU0ZGMwYTNkNzZlYzk3NDNhNGI2Iiwic3RpcCI6IjQ3LjIxNy41Mi4yMCJ9.4KRGoxzm3rfvHIzO3EAZvNZwefHccirjfhjt6DtS7bM/All_Themes_Pack.zip
+unar All_Themes_Pack.zip
+cd All_Themes_Pack
+mv ChromeOS\ Dark\ Lite /home/SkywalkerSW5/.local/share/plank/themes
+cd
+cd .themes
+wget https://dllb2.pling.com/api/files/download/j/eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjE1NzcxODIzNTQiLCJ1IjpudWxsLCJsdCI6ImRvd25sb2FkIiwicyI6ImFmNGU4MTY0MTEwYjZjZGE4YThmM2Q1MzRmNTAxNjYzNGVlZTE2ZDg0MzRiZjgyOWY0YzFkZTFjNjlmNzczY2MwOTBhNGE5MzhmMTYzYWY1MjNiYTA3NDUxZmZjYzdjN2Q2NzI3ODNhODY2YzAyMjJkMTk2YjNmMTlhMzdiZTYyIiwidCI6MTU4NzU4MDYyOSwic3RmcCI6ImUwNzEwNDVjN2Y5ZWU0ZGMwYTNkNzZlYzk3NDNhNGI2Iiwic3RpcCI6IjQ3LjIxNy41Mi4yMCJ9.HRZKCUPfST8dQ1Zo5LWWoDPax9ZMKu72p8-5Vc8pJrM/05-Flat-Remix-GTK-Blue-Dark_20191224.tar.xz
+unar 05-Flat-Remix-GTK-Blue-Dark_20191224.tar.xz
 zsh
 sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
