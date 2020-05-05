cce8c2767d425a3964b495ecdf7575870bb2f40a
SkywalkerSW5 <50605573+SkywalkerSW5@users.noreply.github.com>
2020-05-05 18:02:38 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -24,5 +24,12 @@ curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt-get update  
 sudo apt install mono-complete
+cd
+cd
+cd
+cd .compiled
+cd calming-setup
+sudo mv background.png /home/SkywalkerSW5
+sudo mv startup /home/SkywalkerSW5
 
 
